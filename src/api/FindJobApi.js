43d2189d03da5a262fdafeb3335253cb7fff6a16
ankitbhurji <bhurji204@gmail.com>
2023-03-16 13:12:21 +0530

    update url

--- src/api/FindJobApi.js
+++ src/api/FindJobApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function FindJobsApi1(skills) {
     // const url = `http://localhost:3001/user/findjobs1/${skills}`
-    const url = `https://job-area-backend.onrender.com/user/findjobs1/${sk}`
+    const url = `https://job-area-backend.onrender.com/user/findjobs1/${skills}`
     const getdata = await axios.get(url)
     return getdata
 }
